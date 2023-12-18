78d9d5699eb923f5904d1afb9c33b6aa05f90d7c
Dima <dhasanov2006@mail.ru>
2023-12-18 16:10:26 +0500

    chek

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
-
 from flask import Flask, request, make_response
 import flask
 import json
 import os
 from flask import send_from_directory
+
 app = Flask(__name__)
 
 name = "map.json"
+
 
 @app.route("/videos")
 def get():
     json_file = open(name, 'r')
     videos_json = json.load(json_file)
-    
+
     response = flask.make_response(videos_json)
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
@@ -25,20 +26,21 @@ def upload_file():
     filename = file.filename
     title = request.form['title']
     path_to_save = name
+    if not os.path.exists('storage'):
+        os.makedirs('storage')
     file.save(os.path.join('storage', filename))
     json_file = open(name, 'r')
     my_json = json.load(json_file)
     my_json['videos'].append({'name': title,
                               'description': description,
-                              'manifest_url': 'http://127.0.0.1:5000/play/'+filename})
+                              'manifest_url': 'http://127.0.0.1:5000/play/' + filename})
     with open(name, 'w') as fp:
         json.dump(my_json, fp)
 
     response = make_response("uploaded")
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
-    
-    
+
 
 @app.route('/play/<filename>')
 def play(filename):
@@ -48,7 +50,6 @@ def play(filename):
         print("No such file")
         return flask.make_response("404 not found", 404)
 
-    
 
 if __name__ == "__main__":
     app.run()
