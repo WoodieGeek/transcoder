4622397cdd7d273b646542b9cda5dd79d1faf146
Dima <dhasanov2006@mail.ru>
2023-12-18 14:47:00 +0500

    change upload

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 import json
 import os
 app = Flask(__name__)
@@ -12,16 +12,25 @@ def get():
     return videos_json
 
 
-@app.route('/upload')
-def upload_file(request):
+@app.route('/upload', methods=['POST'])
+def upload_file():
     file = request.files['file']
     description = request.form['description']
     filename = file.filename
-    description_name = description.filename
+    title = request.form['title']
     path_to_save = name
-    file.save(os.path.join(path_to_save, {filename : description_name}))
+    file.save(os.path.join('storage', filename))
+    json_file = open(name, 'r')
+    my_json = json.load(json_file)
+    my_json['videos'].append({'name': title,
+                              'description': description,
+                              'manifest_url': 'http://127.0.0.1:5000/play/'+filename})
+    with open(name, 'w') as fp:
+        json.dump(my_json, fp)
 
+    response = make_response("uploaded")
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 if __name__ == "__main__":
     app.run()
-
