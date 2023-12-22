cb3412ce7327701d7dcbc1239680aa8db5b47a70
Dima <dhasanov2006@mail.ru>
2023-12-22 15:06:11 +0500

    add run_cpp

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ import flask
 import json
 import os
 from flask import send_from_directory
+from moviepy.editor import VideoFileClip
+import datetime
+import subprocess
+import concurrent.futures
 
 app = Flask(__name__)
 
@@ -18,6 +22,9 @@ def get():
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
+def run_cpp(input_file, output_file, start_pos, end_pos):
+    subprocess.run("main", input_file, output_file, start_pos, end_pos)
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -25,7 +32,6 @@ def upload_file():
     description = request.form['description']
     filename = file.filename
     title = request.form['title']
-    path_to_save = name
     if not os.path.exists('storage'):
         os.makedirs('storage')
     file.save(os.path.join('storage', filename))
@@ -37,6 +43,28 @@ def upload_file():
     with open(name, 'w') as fp:
         json.dump(my_json, fp)
 
+    full_video = name
+    full_duration = VideoFileClip(full_video).duration
+    parts = 2
+    part_duration = full_duration/parts
+    start_pos = 0
+    index = 1
+    query = []
+    while (True) :
+        end_pos = start_pos+part_duration
+        if end_pos > full_duration:
+            end_pos = full_duration
+        part_name = "part_"+str(index)+".mp4"
+        os.makedirs('part_name')
+        query.append({full_video, part_name, start_pos, end_pos})
+        start_pos = end_pos
+        index += 1
+        if start_pos >= full_duration:
+            break
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(run_cpp, full_video, part_name, start_pos, end_pos)
+        file.save(os.path.join('storage', part_name))
     response = make_response("uploaded")
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
