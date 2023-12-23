cf094799bc363b189b66753e19bbf2c62e86cefd
raystelle-git <olymp.raystelle@gmail.com>
2023-12-23 21:05:11 +0300

    Something strange

--- frontend/playVideo.js
+++ frontend/playVideo.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.table tr').forEach(function(row) {
   row.addEventListener('click', function() {
     var manifest = this.children[2].textContent;
-    // console.log(manifest);
+    console.log('!!! ' + manifest);
 
     const videoPlayer = document.createElement('video');
     videoPlayer.src = manifest;
