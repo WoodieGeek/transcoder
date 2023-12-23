9270472b385a8797dc2d5e913c8af92e11c3289b
raystelle-git <olymp.raystelle@gmail.com>
2023-12-23 12:06:38 +0300

    Fixed frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,31 @@
 var $table = $('.table');
 
+async function playVideo(url) {
+    const videoPlayerContainer = document.getElementById("video-player");
+    console.log(videoPlayerContainer);
+    videoPlayerContainer.innerHTML = ""; // Clear previous video
+    
+    const video = document.createElement("video");
+    video.controls = true;
+    videoPlayerContainer.appendChild(video);
+    
+    if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+          video.play();
+        });
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = url;
+        video.addEventListener("loadedmetadata", function () {
+          video.play();
+        });
+    } else {
+        console.error("HLS playback is not supported in this browser.");
+    }
+}
+
 async function buildTable() {
     let url = "http://localhost:5000/videos";
     let response = await fetch(url);
@@ -18,7 +44,13 @@ async function buildTable() {
     data = data["videos"];
     for (var i = 0; i < data.length; ++i) {
         $table.append('<tr> <td class = "name">' + data[i].name + '</td> <td class = "description">' + data[i].description + '</td> <td> <a href = ' + data[i].manifest_url + ' class = "table-link"> Link </a> </td> </tr>');
+
+	console.log(data[i].manifest_url);
+        $table[$table.length - 1].addEventListener('click', () => {
+            playVideo(data[$table.length - 1].manifest_url);
+        });
     }
 }
 
 buildTable();
+
