ef0772a3fcb172719aa447c7b26a7b9955536211
Azat <nizamutdinovazat26@gmail.com>
2023-12-23 09:50:25 +0500

    q

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import flask
 import json
 import os
 from flask import send_from_directory
-from moviepy.editor import VideoFileClip
+#from moviepy.editor import VideoFileClip
 import datetime
 import subprocess
 import concurrent.futures
@@ -26,6 +26,19 @@ def run_cpp(input_file, output_file, start_pos, end_pos):
     subprocess.run("main", input_file, output_file, start_pos, end_pos)
 
 
+def manifest_generate(filename, segments_mas):
+    print(segments_mas)
+    with open("storage/processed/" + filename + ".m3u8", 'w') as file_ts:
+        file_ts.write("#EXTM3U\n")
+        file_ts.write("#EXT-X-VERSION:3\n")
+        file_ts.write("#EXT-X-TARGETDURATION:" + str(segments_mas[0][1]) + '\n')
+        file_ts.write("#EXT-X-MEDIA-SEQUENCE:0\n")
+        file_ts.write("#EXT-X-PLAYLIST-TYPE:VOD\n")
+        for i in range(len(segments_mas)):
+            file_ts.write("#EXTINF:" + str(segments_mas[i][1]) + ',\n')
+            file_ts.write(segments_mas[i][0] + '\n')
+
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['file']
@@ -39,45 +52,32 @@ def upload_file():
     my_json = json.load(json_file)
     my_json['videos'].append({'name': title,
                               'description': description,
-                              'manifest_url': 'http://127.0.0.1:5000/play/' + filename})
+                              'manifest_url': 'http://127.0.0.1:5000/play/' + filename[:4] + ".m3u8"})
     with open(name, 'w') as fp:
         json.dump(my_json, fp)
 
-    full_video = name
-    full_duration = VideoFileClip(full_video).duration
-    parts = 2
-    part_duration = full_duration/parts
-    start_pos = 0
-    index = 1
-    query = []
-    while (True) :
-        end_pos = start_pos+part_duration
-        if end_pos > full_duration:
-            end_pos = full_duration
-        part_name = "part_"+str(index)+".mp4"
-        os.makedirs('part_name')
-        query.append({full_video, part_name, start_pos, end_pos})
-        start_pos = end_pos
-        index += 1
-        if start_pos >= full_duration:
-            break
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.submit(run_cpp, full_video, part_name, start_pos, end_pos)
-        file.save(os.path.join('storage', part_name))
+    manifest_generate(filename[:4], [('storage/processed/out1.ts', 9)])
     response = make_response("uploaded")
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 
-@app.route('/play/<filename>')
-def play(filename):
-    if os.path.isfile('storage/' + filename) == True:
-        return send_from_directory('storage', filename)
+
+@app.route('/play/<path:path>')
+
+def play(path):
+    print(path)
+    if os.path.isfile('storage/processed/' + path) == True:
+        response = send_from_directory('storage/processed/', path)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
     else:
+
         print("No such file")
         return flask.make_response("404 not found", 404)
 
+
+        
 
 if __name__ == "__main__":
     app.run()
